f801c8375c44268af1bfba5853cfcc2a0a5db5ee
Nicola Vandyar <nvandyar@gmail.com>
2024-06-21 15:15:55 +0200

    Modified helloWorld.js to prompt for player name and print it

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,11 @@
-console.log("Git is Awesome!");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter player name: ', (playerName) => {
+  console.log(`Player name is: ${playerName}`);
+  rl.close();
+});
